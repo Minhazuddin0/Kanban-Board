47b221708f831185f55374285ec627803305bbd1
Minhaz Uddin <minhazrock1@gmail.com>
2026-04-16 16:47:52 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
+
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    task.addEventListener("drag", (e)) => {
+        console.log("dragging", e);
+    }
+})
+
+
+progress.addEventListener("ondragenter ". (e) =>{
+    
+})
