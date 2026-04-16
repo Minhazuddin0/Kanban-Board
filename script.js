352403bdfd16b52d707fb0221a75a7fe2bcfd9b2
Minhaz Uddin <minhazrock1@gmail.com>
2026-04-16 17:16:50 +0530

    update

--- script.js
+++ script.js
@@ -2,16 +2,28 @@ const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
 
+console.log(todo, progress, done);
 
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(task => {
-    task.addEventListener("drag", (e)) => {
-        console.log("dragging", e);
-    }
+    task.addEventListener("drag", (e) => {
+        // console.log("dragging", e);
+    })
 })
 
 
-progress.addEventListener("ondragenter ". (e) =>{
-    
-})
+function addDragEventsOnColumn(column){
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
